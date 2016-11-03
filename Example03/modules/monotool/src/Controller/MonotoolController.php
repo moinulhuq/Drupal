bbf057b8f72af697f07a3b76f0d1a36200033791
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2016-11-04 11:03:07 +1300

    Create MonotoolController.php

---
+++ Example03/modules/monotool/src/Controller/MonotoolController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @file
+* Contains \Drupal\monotool\Controller\MonotoolController class.
+*/
+namespace Drupal\monotool\Controller;
+use Drupal\Core\Controller\ControllerBase;
+/**
+* Returns responses for My Module module.
+*/
+class MonotoolController extends ControllerBase {
+	/**
+	* Returns markup for our custom page.
+	*/
+	public function customPage() {
+		return [ '#markup' => t('Welcome to my custome page '), ];
+	}
+}
