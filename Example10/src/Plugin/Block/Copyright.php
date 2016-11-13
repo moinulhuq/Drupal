594830e8acfeb42f16d7b6997f6f6ebf2be75b63
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2016-11-14 12:18:15 +1300

    Add files via upload

---
+++ Example10/src/Plugin/Block/Copyright.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* @file
+* Contains \Drupal\monotool\Plugin\Block\Copyright
+*/
+namespace Drupal\monotool\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+/**
+* @Block(
+* id = "copyright_block",
+* admin_label = @Translation("Copyright"),
+* category = @Translation("Custom")
+* )
+*/
+class Copyright extends BlockBase {
+	/**
+	* {@inheritdoc}
+	*/
+	public function build() {
+		$date = new \DateTime();		
+		return [
+				'#markup' => t('Copyright @year&copy; My Company', [ 
+				'@year' => $date->format('Y'),]),
+				];
+	}
+}
