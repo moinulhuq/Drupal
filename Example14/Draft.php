d7d26caf83503899ae84e214222bb83845af3343
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2017-02-26 18:15:05 +1300

    Update Draft.php

--- Example14/Draft.php
+++ Example14/Draft.php
@@ -1,15 +1,22 @@
-#To get nid of all nodes
+#To get nid of all nodes.
   $nids = \Drupal::entityQuery('node')->execute();
 
-#To get all node types (or) entities array
+#To get all node types (or) entities array.
   $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple();
 
-#To get a node content type (or) entity type
+#To get one node types (or) entity array.
+  $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->load(1);
+
+#To get one node content type (or) entity type (Article, Basic page).
   $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple()[1]->bundle();
   $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->load(1)->bundle();
 
-#To get id of only 'Basic page' (or) 'Article' among all nids
+#To get id of only 'Basic page' (or) 'Article' from all nids
   $nids = \Drupal::entityTypeManager()->getStorage('node')->getQuery()->condition('type', 'article')->execute();
+
+#To get one node types (or) entity array of 'Article'.
+  $nids = \Drupal::entityTypeManager()->getStorage('node')->getQuery()->condition('type', 'article')->execute();
+  $articles = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($nids);
 
 #To create node of article type
 	$node = \Drupal::entityTypeManager()->getStorage('node')->create(array('type' => 'article', 'title' => 'Another node'));
