8b636dfb26963e3d344c8c4c24abc83dd3943f8c
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2017-02-26 20:03:43 +1300

    Update Draft2.php

--- Example14/Draft2.php
+++ Example14/Draft2.php
@@ -13,3 +13,11 @@
 		->condition('status', 1)
 		->execute();
 
+#To get user 'moin' from 'users' table
+
+  $query = \Drupal::entityQuery('user')
+		->condition('name', 'moin', 'CONTAINS')
+		->condition('roles', 'Administrator', 'CONTAINS')
+		->execute();
+
+#To
