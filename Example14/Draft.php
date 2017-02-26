be371502926ac1266bc04f499e575a776499dcd3
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2017-02-26 18:05:26 +1300

    Update Draft.php

--- Example14/Draft.php
+++ Example14/Draft.php
@@ -1,5 +1,20 @@
-To get all nid of node
-  $result = \Drupal::entityQuery('node')->execute();
-To create node of article type
+#To get nid of all nodes
+  $nids = \Drupal::entityQuery('node')->execute();
+
+#To get all node types (or) entities array
+  $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple();
+
+#To get a node content type (or) entity type
+  $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple()[1]->bundle();
+  $entity_storage = \Drupal::entityTypeManager()->getStorage('node')->load(1)->bundle();
+
+#To get id of only 'Basic page' (or) 'Article' among all nids
+  $nids = \Drupal::entityTypeManager()->getStorage('node')->getQuery()->condition('type', 'article')->execute();
+
+#To create node of article type
 	$node = \Drupal::entityTypeManager()->getStorage('node')->create(array('type' => 'article', 'title' => 'Another node'));
 	$node->save();
+
+
+https://www.drupal.org/docs/8/api/entity-api/working-with-the-entity-api
+http://www.qed42.com/blog/url-alias-update-using-batch-api-drupal-8
